e43a104e96b93de41c5dff4704104d0a0a8d048d
Mohammad Mansour <mohammed.i02@hotmail.com>
2023-09-15 01:42:31 +0300

    make the add button functional

--- mainscript.js
+++ mainscript.js
@@ -1,31 +1,56 @@
-let addButton = document.getElementById('add-task-button');
-let task = document.getElementById('task-text');
-let tableBody = document.getElementById('table-body');
+let idCount = 0;
 
 fetch('https://dummyjson.com/todos')
-    .then(response => {
+
+  .then(response => {
     if (!response.ok) {
       throw new Error(`Network response was not ok: ${response.status}`);
     }
 
-    return response.json();
+  return response.json();
 
   }).then(data => {
     const todos = data.todos;
 
-    for (let tododata of todos) {
-      let id = tododata.id;
-      let task = tododata.todo;
-      let status = tododata.completed;
-      let userId = tododata.userId;
+    for (let todoData of todos) {
+      idCount++;
+      let id = todoData.id;
+      let task = todoData.todo;
+      let status = todoData.completed;
+      let userId = todoData.userId;
 
       addTask(id,task,userId,status);
     }
 
   }).catch(error => {
     console.error('There was a problem with the fetch operation:', error);
-  });
+  }
+);
 
+let addButton = document.getElementById('add-task-button');
+let task = document.getElementById('task-text');
+let tableBody = document.getElementById('table-body');
+
+addButton.addEventListener('click', (event) =>{
+  event.preventDefault();
+
+  let newTask = task.value;
+
+  if(newTask.trim() !== ''){
+    idCount++;
+    let userId = Math.floor(Math.random() * 100) + 1;
+    addTask(idCount,newTask,userId,'false');
+
+  }else{
+    task.classList.add('shake');
+    setTimeout(() => {
+      task.classList.remove('shake');
+    }, 500);
+  }
+  
+  task.value = '';
+
+} );
 
 function addTask(id,task,userId,status){
 
